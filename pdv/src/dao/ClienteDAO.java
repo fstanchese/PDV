9bc627446cf3590b02def1895fcbd08fa66a3a82
Fernando Stanchese <fstanchese@gmail.com>
2016-03-17 02:26:43 -0300

    alteracoes

--- pdv/src/dao/ClienteDAO.java
+++ pdv/src/dao/ClienteDAO.java
@@ -2,32 +2,70 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import modelo.Cliente;
+import dto.ClienteDTO;
 import util.ConnectionFactory;
 
 public class ClienteDAO {
 
-	public void salvar(Cliente cliente) {
-		String sqlInsert = "INSERT INTO cliente( nome, fone, cpf ) VALUES (?, ?, ?)";
-		
-		try (Connection conn = ConnectionFactory.obtemConexao();
-			PreparedStatement stm = conn.prepareStatement(sqlInsert);) {
-			stm.setString(1, cliente.getNome());
-			stm.setString(2, cliente.getFone());
-			stm.setString(3, cliente.getCpf());
+	public void incluir(ClienteDTO dto) {
+		String sql = "INSERT INTO cliente( nome, fone, cpf ) VALUES (?, ?, ?)";
+
+		try (Connection conn = ConnectionFactory.obtemConexao(); PreparedStatement stm = conn.prepareStatement(sql);) {
+			stm.setString(1, dto.getNome());
+			stm.setString(2, dto.getFone());
+			stm.setString(3, dto.getCpf());
 			stm.execute();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void alterar(Cliente cliente) {
+	public void alterar(ClienteDTO dto) {
+		String sql = "UPDATE cliente SET nome=? , fone=?, cpf=? WHERE id=?";
 
+		try (Connection conn = ConnectionFactory.obtemConexao(); PreparedStatement stm = conn.prepareStatement(sql);) {
+			stm.setString(1, dto.getNome());
+			stm.setString(2, dto.getFone());
+			stm.setString(3, dto.getCpf());
+			stm.setLong(4, dto.getId());
+			stm.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public void excluir(Cliente cliente) {
+	public void excluir(ClienteDTO dto) {
+		String sql = "DELETE FROM cliente WHERE id=?";
 
+		try (Connection conn = ConnectionFactory.obtemConexao(); PreparedStatement stm = conn.prepareStatement(sql);) {
+			stm.setLong(1, dto.getId());
+			stm.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ClienteDTO carregar(Long id) {
+		ClienteDTO dto = new ClienteDTO();
+		String sqlSelect = "SELECT nome, fone, cpf FROM cliente WHERE cliente.id = ?";
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			stm.setLong(1, id);
+			try (ResultSet rs = stm.executeQuery();) {
+				if (rs.next()) {
+					dto.setNome(rs.getString("nome"));
+					dto.setFone(rs.getString("fone"));
+					dto.setCpf(rs.getString("cpf"));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			System.out.print(e1.getStackTrace());
+		}
+		return dto;
 	}
 }
--- pdv/src/modelo/Cliente.java
+++ pdv/src/modelo/Cliente.java
@@ -1,34 +1,85 @@
 package modelo;
 
+import dao.ClienteDAO;
+import dto.ClienteDTO;
+
 public class Cliente {
 	private Long id;
 	private String nome;
 	private String fone;
 	private String cpf;
-	
+
+	public Cliente(Long id, String nome, String fone, String cpf) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.fone = fone;
+		this.cpf = cpf;
+	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getFone() {
 		return fone;
 	}
+
 	public void setFone(String fone) {
 		this.fone = fone;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
+	public void criar() {
+		ClienteDAO dao = new ClienteDAO();
+		ClienteDTO dto = new ClienteDTO();
+		dto.setId(id);
+		dto.setNome(nome);
+		dto.setFone(fone);
+		dto.setCpf(cpf);
+		dao.incluir(dto);
+	}
 	
+	public void alterar() {
+		ClienteDAO dao = new ClienteDAO();
+		ClienteDTO dto = new ClienteDTO();
+		dto.setId(id);
+		dto.setNome(nome);
+		dto.setFone(fone);
+		dto.setCpf(cpf);
+		dao.alterar(dto);		
+	}
+	
+	public void excluir() {
+		ClienteDAO dao = new ClienteDAO();
+		ClienteDTO dto = new ClienteDTO();
+		dto.setId(id);
+		dao.excluir(dto);		
+	}
+	
+	public void carregar() {
+		ClienteDAO dao = new ClienteDAO();
+		ClienteDTO dto = dao.carregar(id);
+		nome = dto.getNome();
+		fone = dto.getFone();
+	}
 }
